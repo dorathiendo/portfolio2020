9c7ea084ebfadfff90371968b2bf0285e5a3edcc
Dora Do <>
2020-07-21 14:31:00 -0700

    Updates

---
+++ precache-manifest.fc78210b555005a9892c2fe1952d101e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b74620b6fa3b2184eca8a33d644ac9f",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "843dac291ffff82ee4cb",
+    "url": "/portfolio2020/static/css/main.6ec9ca67.chunk.css"
+  },
+  {
+    "revision": "4ba6ee49644eaffc1d5e",
+    "url": "/portfolio2020/static/js/2.c3047c94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio2020/static/js/2.c3047c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843dac291ffff82ee4cb",
+    "url": "/portfolio2020/static/js/main.185f9727.chunk.js"
+  },
+  {
+    "revision": "ba59b5319314777654ef",
+    "url": "/portfolio2020/static/js/runtime-main.99f84d1c.js"
+  },
+  {
+    "revision": "0a55a67ceb070cb3e922c92f73a42780",
+    "url": "/portfolio2020/static/media/1.0a55a67c.jpg"
+  },
+  {
+    "revision": "84aad6b415511ad3a0435695cbf3f94f",
+    "url": "/portfolio2020/static/media/2.84aad6b4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
